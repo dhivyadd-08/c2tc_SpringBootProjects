0d377f302176ae735137cd9ebfd59650dc18cf0b
Hp <Hp@LAPTOP-KNQCG56I>
2025-08-16 15:38:54 +0530

    SpringBootProject

---
+++ CourseManagementModule-1/src/main/java/com/tnsif/Coursemanagementmodule1/CourseManagementModule1Application.java
@@ -0,0 +1,13 @@
+package com.tnsif.Coursemanagementmodule1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManagementModule1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseManagementModule1Application.class, args);
+	}
+
+}
